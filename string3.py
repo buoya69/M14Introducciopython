9d29b53afc14eece8ecbcb38c2e4040535649b96
buoya69 <158280861+buoya69@users.noreply.github.com>
2024-02-08 13:20:38 +0100

    Create string3.py

---
+++ string3.py
@@ -0,0 +1,8 @@
+while True:
+    tel = input("Digues el teu umero de telefon")
+
+    if len(tel) == 9:
+        print("El teu telefon es correcte")
+        break
+    else:
+        print("El telefon es incorrecte")
