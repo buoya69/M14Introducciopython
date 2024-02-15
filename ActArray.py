517d8c5f737392c21d5fe34f9a540dada52ccf38
buoya69 <158280861+buoya69@users.noreply.github.com>
2024-02-15 13:56:54 +0100

    Create ActArray.py

---
+++ ActArray.py
@@ -0,0 +1,9 @@
+comunitat = ["Gandalf", "Legolas", "Gimli","Aragorn","Merry","Pippin","Frodo","Sam","Boromir"]
+print(comunitat[0])
+print(comunitat[int(len(comunitat) / 2)])
+comunitat.sort()
+print(comunitat[0])
+print(comunitat[-1])
+comunitat.remove("Aragorn")
+print(comunitat[0])
+comunitat.append("Arwen")
