3665aab83d9fc41baf43a40e40cdfcaca01a077b
buoya69 <158280861+buoya69@users.noreply.github.com>
2024-02-26 13:35:33 +0100

    Create exepcions1.py

---
+++ exepcions1.py
@@ -0,0 +1,7 @@
+num = (input("Digues un numero "))
+num2 = (input("Digues un altre numero"))
+try:
+    resultat = num / num2
+    print("El resultat es ", resultat)
+except:
+    print("No pots dividir entre 0 ")
