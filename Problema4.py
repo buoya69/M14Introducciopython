148c5850c3311c25102e7f95cb598f2ff771c3f7
buoya69 <158280861+buoya69@users.noreply.github.com>
2024-02-29 10:01:06 +0100

    Create Problema4.py

---
+++ Problema4.py
@@ -0,0 +1,13 @@
+nota = int(input("Quina nota tens? "))
+if nota > 4:
+    print("Insuficient")
+if nota == 5:
+    print("Suficient")
+if nota == 6:
+    print("Be")
+if nota == 7:
+    print("Notable")
+if nota == 8:
+    print("notable")
+if nota == 9 or nota == 10:
+    print("Exelent")
